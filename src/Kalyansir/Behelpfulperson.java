09f03ecaed1f12babbb061e003b0137131ce5b76
shoba palakolanu <Shoba Palakolanu>
2024-07-04 21:06:03 +0530

    no one take care of you

---
+++ src/Kalyansir/Behelpfulperson.java
@@ -0,0 +1,13 @@
+package Kalyansir;
+
+public class Behelpfulperson {
+
+	public static void main(String[] args) 
+	{
+      System.out.println("Our main agenda is teach core "
+      		+ "functionality to students like as primary colors");
+      System.out.println("arif is a wise person");
+
+	}
+
+}
